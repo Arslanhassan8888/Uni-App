f9f8313ddccdfc113b61ba12e1e93460a660ee99
Arslanhassan8888 <arslanarslan84@yahoo.com>
2026-04-18 18:13:19 +0100

    Implemented Person class with fields, constructors and methods, and tested functionality in main class

--- src/main/java/com/mycompany/uni_app_arslan/Person.java
+++ src/main/java/com/mycompany/uni_app_arslan/Person.java
@@ -1,9 +1,141 @@
 package com.mycompany.uni_app_arslan;
 
-/**
- * The Person class represents a general person in the university halls system.
- *
- * @author Arslan Hassan
- */
+/* Person class represents a general person in the university halls system.
+
+   This class is a parent class.
+   It stores common information shared by students and employees.
+*/
 public class Person {
+
+    // The full name of the person
+    private String name;
+
+    // The gender of the person
+    private String gender;
+
+    // The person's date of birth
+    private String dateOfBirth;
+
+    // The home address of the person
+    private String address;
+
+    // The nationality of the person
+    private String nationality;
+
+    // Any health conditions
+    private String healthConditions;
+
+    // The date when the record was added to the system
+    private String registrationDate;
+
+    // Constructors
+
+    /* Default constructor.
+       Creates a Person object with no initial values.
+    */
+    public Person() {
+    }
+
+    /* Full constructor.
+       Creates a Person object with all fields set at once.
+    */
+    public Person(String name, String gender, String dateOfBirth, String address,
+                  String nationality, String healthConditions, String registrationDate) {
+
+        this.name = name;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.nationality = nationality;
+        this.healthConditions = healthConditions;
+        this.registrationDate = registrationDate;
+    }
+
+    // Getters and setters
+
+    /* Gets the person's name. */
+    public String getName() {
+        return name;
+    }
+
+    /* Sets the person's name. */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /* Gets the person's gender. */
+    public String getGender() {
+        return gender;
+    }
+
+    /* Sets the person's gender. */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /* Gets the person's date of birth. */
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /* Sets the person's date of birth. */
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /* Gets the person's address. */
+    public String getAddress() {
+        return address;
+    }
+
+    /* Sets the person's address. */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /* Gets the person's nationality. */
+    public String getNationality() {
+        return nationality;
+    }
+
+    /* Sets the person's nationality. */
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    /* Gets the person's health conditions. */
+    public String getHealthConditions() {
+        return healthConditions;
+    }
+
+    /* Sets the person's health conditions. */
+    public void setHealthConditions(String healthConditions) {
+        this.healthConditions = healthConditions;
+    }
+
+    /* Gets the registration date. */
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    /* Sets the registration date. */
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    // toString method
+
+    /* Returns a readable version of the object. */
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", address='" + address + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", healthConditions='" + healthConditions + '\'' +
+                ", registrationDate='" + registrationDate + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/mycompany/uni_app_arslan/Uni_App_Arslan.java
+++ src/main/java/com/mycompany/uni_app_arslan/Uni_App_Arslan.java
@@ -1,12 +1,24 @@
 package com.mycompany.uni_app_arslan;
 
-/**
- *
- * @author Arslan Hassan
- */
+/*
+ Main class for testing the application.
+*/
 public class Uni_App_Arslan {
 
     public static void main(String[] args) {
 
+        // Create a Person object
+        Person p = new Person(
+                "Arslan Hassan",
+                "Male",
+                "01/01/2000",
+                "Manchester",
+                "British",
+                "None",
+                "18/04/2026"
+        );
+
+        // Print the object
+        System.out.println(p);
     }
 }
