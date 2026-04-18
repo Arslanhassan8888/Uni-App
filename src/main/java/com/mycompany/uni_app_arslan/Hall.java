cd78d8b1c1fdc024bc4fcd151a8fea47f145098b
Arslanhassan8888 <arslanarslan84@yahoo.com>
2026-04-18 20:50:08 +0100

    Added getters and setters for Employee class fields

--- src/main/java/com/mycompany/uni_app_arslan/Hall.java
+++ src/main/java/com/mycompany/uni_app_arslan/Hall.java
@@ -1,9 +1,30 @@
 package com.mycompany.uni_app_arslan;
 
 /**
- * The Hall class represents a hall of residence.
  *
  * @author Arslan Hassan
  */
+
+/*
+ Hall class represents a hall of residence in the university halls system.
+
+ This class stores information about a hall.
+*/
 public class Hall {
+
+    // Name of the hall
+    private String hallName;
+
+    // Type of hall (e.g. mixed, female only, quiet)
+    private String hallType;
+
+    // Maximum number of residents
+    private int capacity;
+
+    // Is the hall suitable for vegetarian students?
+    private boolean vegetarianFriendly;
+
+    // Does the hall have ground floor rooms?
+    private boolean groundFloorAvailable;
 }
+
