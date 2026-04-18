77d083420976e8675a92ad89a947e02ca18d63e8
Arslanhassan8888 <arslanarslan84@yahoo.com>
2026-04-18 19:49:33 +0100

    Added employee fields (employeeId, jobRole, salary, hallName) to Employee class

--- src/main/java/com/mycompany/uni_app_arslan/Employee.java
+++ src/main/java/com/mycompany/uni_app_arslan/Employee.java
@@ -5,5 +5,24 @@ package com.mycompany.uni_app_arslan;
  *
  * @author Arslan Hassan
  */
-public class Employee {
+/*
+ Employee class represents a staff member in the university halls system.
+
+ This class extends Person.
+ It will inherit common information from the Person class.
+*/
+public class Employee extends Person {
+
+    // Employee ID
+    private String employeeId;
+
+    // Job role (e.g. cleaner, manager, security)
+    private String jobRole;
+
+    // Salary of the employee
+    private double salary;
+
+    // Hall name where the employee works or lives
+    private String hallName;
+
 }
